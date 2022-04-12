37d81bd8515244e3999c9f391e87e6f0874c61e8
Tanimara Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2022-04-12 16:58:24 +1000

    Create Navbar.tsx

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import { User } from "../model/Model";
+import { Link } from "react-router-dom";
+
+export class Navbar extends Component<{
+  user: User | undefined;
+}> {
+  render() {
+    let loginLogOut: any;
+    if (this.props.user) {
+      loginLogOut = <Link to="logout">{this.props.user.userName}</Link>;
+    } else {
+      loginLogOut = <Link to="login">Login</Link>;
+    }
+    return (
+      <div className="navbar">
+        <Link to="/">Home</Link>
+        <Link to="/profile">Profile</Link>
+        {loginLogOut}
+      </div>
+    );
+  }
+}
+
+export default Navbar;
