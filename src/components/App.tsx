4e7c8f02eb1658a26ca0e51bb6b1e96e538c238e
Tanimara Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2022-04-12 16:57:42 +1000

    Update App.tsx
    Import router, routes and switch
    Define constructor
    define private state

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,16 +2,29 @@ import React from "react";
 import { User } from "../model/Model";
 import { AuthService } from "../services/AuthService";
 import { Login } from "./Login";
+import { Router, Route, Switch } from "react-router-dom";
+import history from "../utils/history";
 interface AppState {
   user: User | undefined;
 }
 export class App extends React.Component<{}, AppState> {
   private authService: AuthService = new AuthService();
+
+  constructor(props: any) {
+    super(props);
+
+    this.setUser = this.setUser.bind(this);
+  }
+
+  private setUser(user: User) {
+    this.setState({
+      user: user,
+    });
+  }
   render() {
     return (
-      <div>
-        App from class works!
-        <Login authService={this.authService} />
+      <div className="wrapper">
+        <Router history={history}></Router>
       </div>
     );
   }
