c39bf58f6a03c6d55d6923916dc1b256b1bd2012
Tanimara Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2022-04-12 16:58:29 +1000

    Create Profile.tsx

---
+++ src/components/Profile.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class Profile extends Component {
+  render() {
+    return (
+      <div>Welcome to the profile page!</div>
+    )
+  }
+}
+
+export default Profile
