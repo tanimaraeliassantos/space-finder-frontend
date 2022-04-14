d0bf445fc2e40a3a78716acb5fb35a6078a04fa2
Tanimara Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2022-04-14 11:54:37 +1000

    Update App.tsx
    - Define state for user as undefined
    - Insert navbar
    - Insert Switch component to look through all of children routes and display first one whose path matches the URL
    --Define routes to look through with respective paths
    --Add route for page not found
    ---Create function for component page not found

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,6 +4,10 @@ import { AuthService } from "../services/AuthService";
 import { Login } from "./Login";
 import { Router, Route, Switch } from "react-router-dom";
 import history from "../utils/history";
+import Navbar from "./Navbar";
+import Home from "./Home";
+import Profile from "./Profile";
+
 interface AppState {
   user: User | undefined;
 }
@@ -12,7 +16,9 @@ export class App extends React.Component<{}, AppState> {
 
   constructor(props: any) {
     super(props);
-
+    this.state = {
+      user: undefined,
+    };
     this.setUser = this.setUser.bind(this);
   }
 
@@ -24,10 +30,30 @@ export class App extends React.Component<{}, AppState> {
   render() {
     return (
       <div className="wrapper">
-        <Router history={history}></Router>
+        <Router history={history}>
+          <div>
+            <Navbar user={this.state.user} />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login">
+                <Login authService={this.authService} setUser={this.setUser} />
+              </Route>
+              <Route exact path="/profile">
+                <Profile
+                  authService={this.authService}
+                  user={this.state.user}
+                />
+              </Route>
+              <Route exact path="*" component={notFound} />
+            </Switch>
+          </div>
+        </Router>
       </div>
     );
   }
 }
 
+function notFound() {
+  return <>You have landed on a page that doesn't exist!</>;
+}
 export default App;
