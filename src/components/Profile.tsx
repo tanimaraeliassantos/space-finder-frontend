99e39f04b0821daeb6d57863be25b5d006f7256f
Tanimara Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2022-04-14 12:03:50 +1000

    Update Profile.tsx
    Import Link, User, UserAttribute, AuthService
    
    Define type for ProfileState
    Define type for ProfileProps
    
    Declare component profile to have Props and State
    Initialise State with userAttributes array
    Asynchronously define that if component is mounted with UserAttributes from user
    Then, setState with the new info.
    
    Private define renderUserAttributes starting with rows as an empty array
    Looping over each user attributes to create rows with key, name and value.
    Return table with rows in the body
    Define profileSpace
    If there is a user, then show the content of the div, if not direct users to Login page
    Return welcome to the profile page with the profileSpace component.

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,11 +1,74 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { User, UserAttribute } from "../model/Model";
+import { AuthService } from "../services/AuthService";
 
-export class Profile extends Component {
-  render() {
+interface ProfileState {
+  userAttributes: UserAttribute[];
+}
+
+interface ProfileProps {
+  user: User | undefined;
+  authService: AuthService;
+}
+export class Profile extends Component<ProfileProps, ProfileState> {
+  state: ProfileState = {
+    userAttributes: [],
+  };
+
+  async componentDidMount() {
+    if (this.props.user) {
+      const userAttrs = await this.props.authService.getUserAttributes(
+        this.props.user
+      );
+      this.setState({
+        userAttributes: userAttrs,
+      });
+    } else {
+    }
+  }
+
+  private renderUserAttributes() {
+    const rows = [];
+    for (const userAttribute of this.state.userAttributes) {
+      rows.push(
+        <tr key={userAttribute.Name}>
+          <td>{userAttribute.Name}</td>
+          <td>{userAttribute.Value}</td>
+        </tr>
+      );
+    }
     return (
-      <div>Welcome to the profile page!</div>
-    )
+      <table>
+        <tbody>{rows}</tbody>
+      </table>
+    );
+  }
+
+  render() {
+    let profileSpace;
+    if (this.props.user) {
+      profileSpace = (
+        <div>
+          <h3>Hello {this.props.user.userName}</h3>
+          Here are your attributes:
+          {this.renderUserAttributes()}
+        </div>
+      );
+    } else {
+      profileSpace = (
+        <div>
+          <Link to="login">Login</Link>
+        </div>
+      );
+    }
+    return (
+      <div>
+        Welcome to the profile page!
+        {profileSpace}
+      </div>
+    );
   }
 }
 
-export default Profile
+export default Profile;
